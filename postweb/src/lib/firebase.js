914d0230823acb90e92b84a96d974219b06640a1
Hồng Phương <nglhongphuong@gmail.com>
2025-07-04 16:06:15 +0700

    Update firebase.js

--- postweb/src/lib/firebase.js
+++ postweb/src/lib/firebase.js
@@ -6,13 +6,13 @@ import {getFirestore} from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnDL33IIn0fRIoFhp_RLHDVJsWaBT5uRE",
-  authDomain: "postchat-500ba.firebaseapp.com",
-  projectId: "postchat-500ba",
-  storageBucket: "postchat-500ba.firebasestorage.app",
-  messagingSenderId: "184211836104",
-  appId: "1:184211836104:web:805961d71e74964aa3a9fd",
-  measurementId: "G-T023NWK805"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
